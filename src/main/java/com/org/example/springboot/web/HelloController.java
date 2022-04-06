ed3228885a9777f65296d3125d5aed3f35d04b7f
Jung <qprodfkj@gmail.com>
2022-04-06 18:22:14 +0900

    lombok test

--- src/main/java/com/org/example/springboot/web/HelloController.java
+++ src/main/java/com/org/example/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.org.example.springboot.web;
 
+import com.org.example.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,10 @@ public class HelloController {
     public String hello(){
         return "test_hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
 }
