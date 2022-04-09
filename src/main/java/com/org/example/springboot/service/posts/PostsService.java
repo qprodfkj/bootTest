228282b846d92a83d1b3ae52c3666b421a388b82
Jung <qprodfkj@gmail.com>
2022-04-09 14:47:12 +0900

    게시판 등록/수정/삭제

--- src/main/java/com/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/com/org/example/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.org.example.springboot.service.posts;
 
 import com.org.example.springboot.domain.posts.Posts;
 import com.org.example.springboot.domain.posts.PostsRepository;
+import com.org.example.springboot.web.dto.PostsListResponseDto;
 import com.org.example.springboot.web.dto.PostsResponseDto;
 import com.org.example.springboot.web.dto.PostsSaveRequestDto;
 import com.org.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,11 +31,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)
+        );
+        postsRepository.delete(entity);
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)
         );
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
