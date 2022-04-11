7c885942a6fd7f037ce42a800a2ae766b36d9d82
Jung <qprodfkj@gmail.com>
2022-04-11 18:12:25 +0900

    어노테이션 기반으로 개선

--- src/main/java/com/org/example/springboot/web/IndexController.java
+++ src/main/java/com/org/example/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.org.example.springboot.web;
 
+import com.org.example.springboot.config.auth.LoginUser;
 import com.org.example.springboot.config.auth.dto.SessionUser;
 import com.org.example.springboot.service.posts.PostsService;
 import com.org.example.springboot.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
