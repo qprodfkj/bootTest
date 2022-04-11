dd66a34bf2b0feeb459cf15425220c88d1b2ffca
Jung <qprodfkj@gmail.com>
2022-04-11 17:59:38 +0900

    oauth2 진행중(구글로그인)

--- src/main/java/com/org/example/springboot/web/IndexController.java
+++ src/main/java/com/org/example/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.org.example.springboot.web;
 
+import com.org.example.springboot.config.auth.dto.SessionUser;
 import com.org.example.springboot.service.posts.PostsService;
 import com.org.example.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @Autowired
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
